bff0e0840c7cabf6882767277fcc82c56e1534ba
IT24101143 <tinokani123dilka@gmail.com>
2026-06-16 19:42:41 +0530

    Updated

--- backend/models/Vehicle.js
+++ backend/models/Vehicle.js
@@ -8,6 +8,8 @@ const vehicleSchema = new mongoose.Schema({
     required: true 
   }, // Renter who owns this vehicle
 
+  vehicleId: { type: String, unique: true }, // ✅ optional custom ID
+  
   name: { type: String, required: true },
   type: { type: String, enum: ['car', 'bike', 'van', 'scooter'], required: true },
   pricePerDay: { type: Number, required: true },
@@ -15,7 +17,8 @@ const vehicleSchema = new mongoose.Schema({
   image: { type: String }, // URL or path
   description: String,
   isAvailable: { type: Boolean, default: true },
-  createdAt: { type: Date, default: Date.now }
-});
+},{ timestamps: true });
+  
+  
 
 module.exports = mongoose.model("Vehicle", vehicleSchema);
